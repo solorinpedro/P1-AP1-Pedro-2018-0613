d3215eb6fdc984d85f81c303d9f3a180e4a7155c
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-10-05 12:14:07 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P1_AP1_Pedro_2018_0613
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
