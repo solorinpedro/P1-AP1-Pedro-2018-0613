53d8ad47c0e30a95b4bb6bdc8bb19f67551ef624
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-10-05 12:48:49 -0400

    Creando la BLL de aportes

---
+++ BLL/AportesBLL.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Pedro_2018_0613.DAL;
+using P1_AP1_Pedro_2018_0613.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Pedro_2018_0613.BLL
+{
+    class AportesBLL
+    {
+        public static bool Guardar(Aportes aporte)
+        {
+            if (!Existe(aporte.AportesId))
+                return Insertar(aporte);
+            else
+                return Modificar(aporte);
+        }
+
+        private static bool Insertar(Aportes aporte)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Aportes.Add(aporte);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        private static bool Modificar(Aportes aporte)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(aporte).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var Aporte = contexto.Aportes.Find(id);
+                if (Aporte != null)
+                {
+                    contexto.Aportes.Remove(Aporte);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static Aportes Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Aportes aporte;
+
+            try
+            {
+                aporte = contexto.Aportes.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return aporte;
+        }
+
+        private static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Aportes.Any(e => e.AportesId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        public static List<Aportes> GetList()
+        {
+            List<Aportes> lista = new List<Aportes>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Aportes.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static List<Aportes> GetList(Expression<Func<Aportes, bool>> criterio)
+        {
+            List<Aportes> lista = new List<Aportes>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Aportes.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
