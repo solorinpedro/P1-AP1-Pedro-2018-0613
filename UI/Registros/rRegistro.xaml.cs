e482e9eb4bcfe324135bc8947981c3fd08f934f5
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-10-05 13:30:46 -0400

    Creando diseño  de nuestro registro

---
+++ UI/Registros/rRegistro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Pedro_2018_0613.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rRegistro.xaml
+    /// </summary>
+    public partial class rRegistro : Window
+    {
+        public rRegistro()
+        {
+            InitializeComponent();
+        }
+
+        private void BotonBuscar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BotonNuevo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BotonGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BotonEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
