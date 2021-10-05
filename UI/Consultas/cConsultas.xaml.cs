3f53d01620de65f7218ca53f27bfc0d1e7d2d148
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-10-05 16:31:03 -0400

    Creando diseño de la consulta

---
+++ UI/Consultas/cConsultas.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Pedro_2018_0613.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cConsultas.xaml
+    /// </summary>
+    public partial class cConsultas : Window
+    {
+        public cConsultas()
+        {
+            InitializeComponent();
+        }
+
+ 
+    }
+}
