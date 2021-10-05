14aebb2027350574f137857c8ab0145cc30b700a
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-10-05 12:23:20 -0400

    Creando Clase Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Pedro_2018_0613.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Pedro_2018_0613.DAL
+{
+    public  class Contexto : DbContext 
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\registroAportes.db");
+        }
+    }
+}
